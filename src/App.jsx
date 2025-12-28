0c73213518331b44c20b1192cfbf3869257e1319
Randy <ranidaperera1@gmail.com>
2025-12-28 17:16:05 +0530

    Load property data into application

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import propertiesData from "./data/properties.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  console.log(count, propertiesData);
 
   return (
     <>
