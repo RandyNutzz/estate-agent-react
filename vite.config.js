7a3cf30505a9321c796fcd944322f0cfe08df996
Randy <ranidaperera1@gmail.com>
2026-01-05 08:41:44 +0530

    Update for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 ﻿import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   base: '/estate-agent-react/',
   build: {
     outDir: 'dist',
-    sourcemap: true,
+    emptyOutDir: true,
+    sourcemap: false,
   },
   server: {
     port: 3000,
